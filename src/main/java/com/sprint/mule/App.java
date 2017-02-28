a1640e4d8151a258b26677fd39bfc6d8e6ce2f40
nardevar <nardevar@in.ibm.com>
2017-02-28 08:53:55 +0530

    maven

---
+++ src/main/java/com/sprint/mule/App.java
@@ -0,0 +1,13 @@
+package com.sprint.mule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
